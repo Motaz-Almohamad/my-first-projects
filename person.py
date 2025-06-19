52aa278cfa73e71e8ec1d7a8504ad96604d99644
Motaz-Almohamad <motazalmohammad333@gmail.com>
2025-06-19 19:26:06 +0200

    update

--- person.py
+++ person.py
@@ -1,5 +1,8 @@
+import json
 personen = []
 from  defpersonen import add ,show,search,delet
+with open('pers.json') as pers_file:
+        personen = json.load(pers_file)
 while 1 :
     try:
         p = int(input("\n0.Code stoppen\n1.Person Hinzufügen\n2.Person Anzeigen\n3.Person suchen\n4.Person Löschen\nYour nummber: "))
@@ -14,11 +17,15 @@ while 1 :
         
         case 1:
             add(personen)
+            with open("pers.json","w") as f:
+                json.dump(personen , f)
         case 2:
            show(personen)
         case 3:
             search(personen)
         case 4:
             delet(personen)
+            with open("pers.json","w") as f:
+                json.dump(personen , f)
         case _:
             print("Gib bitte ein zahl zwichen 1 und 4.")
